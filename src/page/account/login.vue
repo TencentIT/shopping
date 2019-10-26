7b7ea890d59ae32dcdfdfa5a6fb4b4d316626e65
吴佳强 <wujiaqiang@icourt.cc>
2019-10-26 20:55:17 +0800

    登录页 logo开发完毕

--- src/page/account/login.vue
+++ src/page/account/login.vue
@@ -1,5 +1,21 @@
+/* eslint-disable no-tabs */
 <template>
-    <div>
-        login.vue
-    </div>
+  <div class="login-box">
+    <div class="m-logo" ></div>
+    <div class="login-btns">
+			<van-button type="primary" size="large">大号按钮</van-button>
+			<van-button type="primary" size="large">大号按钮</van-button>
+		</div>
+  </div>
 </template>
+
+<style lang="stylus">
+.login-box
+  .m-logo
+    background:url(//haitao.nos.netease.com/f866dd18-12f0-4bb2-be6d-5cac85cf2627.png) center 73px no-repeat;
+    background-size:161px;
+    text-align: center;
+    padding: 130px 0 0;
+    margin: 15px 0;
+
+</style>
