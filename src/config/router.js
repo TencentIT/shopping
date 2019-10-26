2a3cfac51761a5c1870632f13680ca23eb138e22
吴佳强 <wujiaqiang@icourt.cc>
2019-10-26 20:29:10 +0800

    配置路由

--- src/config/router.js
+++ src/config/router.js
@@ -10,6 +10,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../page/index')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../page/account/login')
     }
   ]
 })
---
+++ src/page/account/login.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        login.vue
+    </div>
+</template>
